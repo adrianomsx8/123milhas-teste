22659737392912f02bd2e4b6576bf55e4cf4f0cb
Adriano Souza <adriano.msx8@gmail.com>
2022-06-04 15:18:49 -0300

    ajustes criação do grupo e criação do json

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -14,27 +14,36 @@ class ApiController extends Controller
        // $response = Http::get('http://prova.123milhas.net/api/flights');
         $response  = Api::get('flights')->json();
 
+        $grupo1 = $this->getDadosPrimeiroGrupo($response , '1AF');
+        $grupo2 = $this->getDadosPrimeiroGrupo($response , '4DA');
+
+        $result = [];
+        $result['flights'] = $response;
+        $result['groups'] = [$grupo1,  $grupo2];
+        $result['totalGroups'] = count($result['groups'] );
+        $result['cheapestPrice'] = ($grupo1['totalPrice'] < $grupo2['totalPrice'] ? $grupo1['totalPrice'] : $grupo2['totalPrice']);
+        $result['cheapestGroup'] = ($grupo1['totalPrice'] < $grupo2['totalPrice'] ? $grupo1['uniqueId'] : $grupo2['uniqueId']);
+
+        $result['totalFlights'] = count($result['flights'] );
+
+        return response()->json( $result);
+    }
+
+    public function getDadosPrimeiroGrupo($response, $fare){
+
         $outbound = [];
         $inbound = [];
         $totalPrice = [];
-
-        $grupo1 = $this->getDadosPrimeiroGrupo($response, $outbound, $inbound, $totalPrice);
-
-
-       var_dump( $grupo1);
-    }
-
-    public function getDadosPrimeiroGrupo($response, $outbound, $inbound, $totalPrice){
         foreach ($response as $arr){
 
             //voos de ida
-            if($arr['origin'] == 'CNF' && $arr['fare'] == '1AF'){
+            if($arr['origin'] == 'CNF' && $arr['fare'] == $fare){
                 $outbound[] = $arr;
                 $totalPrice[] +=  $arr['price'];
             }
 
             //voos de volta
-            if($arr['origin'] == 'BSB' && $arr['fare'] == '1AF'){
+            if($arr['origin'] == 'BSB' && $arr['fare'] == $fare){
                 $inbound[] = $arr;
                 $totalPrice[] +=  $arr['price'];
             }
